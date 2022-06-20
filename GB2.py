4ca319d891b1f27732803e08ecb56c2e4b3bab91
blnikan <blnikan@mail.ru>
2022-06-20 23:43:37 +0300

    a

--- GB2.py
+++ GB2.py
@@ -10,28 +10,35 @@ from PIL import Image
 from matplotlib import pyplot as plt
 import numpy as np
 import cv2 as cv
-
 #------------------------ CONSTS ------------------------
-
-pix_r = 1
+pix_r = 3
 sigma = 1.5
-
 #------------------------ CONSTS ------------------------
-"""
-
-
-I = cv2.imread('sarajevo.jpg')[:, :, ::-1]
-plt.figure(num=None, figsize=(15, 15), dpi=80, facecolor='w', edgecolor='k')
-plt.imshow(I)
-plt.show()
-"""
-
-#vvvvvvvvvvvvvvvvvvvvvvv  FUNCTIONS  vvvvvvvvvvvvvvvvvvvvvvv
+#------------------------  FUNCTIONS  ------------------------
 
 def gauss_func(sigma_, pos): # Gaussian function (sigma_ - sigma ; pos - coordinate  in Gaussian function)
     return (1 / ( (2 * np.pi * sigma_ ** 2) ** 0.5 ) ) * np.e ** (-((pos ** 2)/(2  * sigma_ ** 2)))
 
-def Weight_Matrix(sigma_): #create weight matrix of radius = 1
+def Weight_Matrix_(sigma_, rad_):
+    
+    pos_x = np.zeros([2*rad_+1,2*rad_+1])
+    pos_y = np.zeros([2*rad_+1,2*rad_+1])
+    tepm_x = -rad_
+    #temp_y = 0
+    for i in range(pos_x.shape[0]):
+        
+        for j in range(pos_x.shape[1]):
+            pos_x[j,i] = tepm_x
+            pos_y[i,j] = -1*tepm_x
+                        
+        tepm_x += 1
+    print("pos_x\n",pos_x)
+    print('pos_y\n',pos_y)
+    res = gauss_func(sigma_,pos_x) * gauss_func(sigma_,pos_y)
+    
+    return res
+    
+def Weight_Matrix_1r(sigma_): #create weight matrix of radius = 1
     
     pos_x = np.array([[-1,0,1],
                      [-1,0,1],
@@ -41,75 +48,81 @@ def Weight_Matrix(sigma_): #create weight matrix of radius = 1
                      [0,0,0],
                      [-1,-1,-1]],dtype= np.int32)
 
-
+    
     res = gauss_func(sigma_,pos_x) * gauss_func(sigma_,pos_y)
     
     return res
     
-#^^^^^^^^^^^^^^^^^^^^^^^ FUNCTIONS ^^^^^^^^^^^^^^^^^^^^^^^^
-
-
+#------------------------ FUNCTIONS ------------------------
 #------------------------ LOAD IMAGE ------------------------
 #im = Image.open('/home/nik/Documents/Gaussian_Blur/pic.jpg')
 #im = np.array(im)
 im = cv.imread('/home/nik/Documents/Gaussian_Blur/pic.jpg')[:,:,::-1]
 im_res =  np.zeros(im.shape)
-
 #------------------------ LOAD IMAGE ------------------------
+#------------------------  ------------------------
+Weight_Matrix = Weight_Matrix_(sigma,pix_r)
 
 
+for i in range (pix_r,im.shape[0] - pix_r):
 
-Weight_Matrix = Weight_Matrix(sigma)
+    for j in range (pix_r,im.shape[1] - pix_r):
+        #0-rojo (red)
+        #1-verde (green)
+        #2-azul (blue)
 
-t = im[:,:,0]
-
-
-for i in range (1,im.shape[0] - 1):
-
-    for j in range (1,im.shape[1] - 1):
 
         temp_small_im = im[i - pix_r : i + 2 * pix_r, j - pix_r : j + 2 * pix_r , : ]
         
-        print(temp_small_im[:,:,0],'\n\n]',Weight_Matrix,'\n\n\n')
-        red_one =  temp_small_im[:,:,0]*Weight_Matrix
-        print('\n',red_one)
+        #red_one =  temp_small_im[:,:,0] * Weight_Matrix  
+        #red_one = int(np.sum(temp_small_im[:,:,0] * Weight_Matrix ))
+        #green_one = temp_small_im[:,:,1] * Weight_Matrix 
+        #green_one = int(np.sum(temp_small_im[:,:,1] * Weight_Matrix ))
+        #blue_one =  temp_small_im[:,:,2] * Weight_Matrix 
+        #blue_one = int(np.sum(temp_small_im[:,:,2] * Weight_Matrix ))
         
-        red_one = int(np.sum(red_one))
-        print(np.sum(red_one,dtype=np.int32))
-        print('\n',red_one)
-        
-        if red_one >255:
-            red_one =255
-        
-        green_one = temp_small_im[:,:,1]*Weight_Matrix 
-        green_one = int(np.sum(green_one))
-        
-        if green_one >255:
-            green_one =255
-        
-        blue_one =  temp_small_im[:,:,2]*Weight_Matrix 
-        blue_one = int(np.sum(blue_one))
+        #if red_one >= 255:
+        #    red_one = 254
+        #if green_one >= 255:
+        #    green_one = 254
+        #if blue_one >= 255:
+        #    blue_one = 254
         
         
         
-        if blue_one >255:
-            blue_one =255
-        break
-        
-        im_res[i,j,0] = red_one#int(np.sum(red_one))
-        im_res[i,j,1] =  blue_one #int(np.sum(green_one))
-        im_res[i,j,2] = green_one#int(np.sum(blue_one))
-        
-    break
-    
+        im_res[i,j,0],im_res[i,j,1],im_res[i,j,2] = int(np.sum(temp_small_im[:,:,0] * Weight_Matrix )),int(np.sum(temp_small_im[:,:,1] * Weight_Matrix )),int(np.sum(temp_small_im[:,:,2] * Weight_Matrix ))
 
 
+
+#------------------------  ------------------------
 #------------------------ OUTPUT ------------------------
-
 #plt.subplot(121),plt.imshow(im)
 #plt.xticks([]), plt.yticks([])
 #plt.subplot(122),plt.imshow(im_res)
 #plt.xticks([]), plt.yticks([])
+
+#plt.figure(num= None, figsize=(10,10),dpi=80)
+#plt.imshow(im.astype(np.uint8))
 plt.figure(num= None, figsize=(10,10),dpi=80)
-plt.imshow(im_res)
+plt.imshow(im_res.astype(np.uint8))
 plt.show()
+
+#new_im = Image.fromarray(im_res.astype(np.uint8))
+#new_im.save('g.jpg')
+
+
+#------------------------ OUTPUT ------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
