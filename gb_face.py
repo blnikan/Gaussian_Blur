37911263d70cd87ac2407192d9ace02d9cfae406
blnikan <blnikan@mail.ru>
2022-10-27 19:32:23 +0300

    fast

--- gb_face.py
+++ gb_face.py
@@ -12,6 +12,7 @@ import numpy as np
 import cv2 
 import dlib
 import face_recognition
+from PIL import Image
 
 
 def gauss_func(sigma_, pos): 
@@ -89,3 +90,67 @@ def GauB(img,rad,sig):
 
 def dec(i0,j0,i1,j1):
     return ( (i0-i1)**2 + (j0-j1)**2 )**0.5
+
+
+
+im = cv2.imread('/home/nik/Documents/Untitled.png', cv2.COLOR_BGR2RGB)
+
+pix_r = 3
+pix_r_MAX = 20
+sigma = 7
+
+
+face_locations = face_recognition.face_locations(im)
+
+print(f'face_locations {face_locations}')
+
+top    = face_locations[0][0]
+right  = face_locations[0][1]
+bottom = face_locations[0][2]
+left   = face_locations[0][3]
+
+center_i = top  + (bottom - top) //2
+center_j = left + (right - left) //2
+
+
+H = im.shape[0]
+W = im.shape[1]
+
+im = padding(im,pix_r_MAX)
+
+
+
+im_res = np.zeros(im.shape)
+
+r_Circle =  (bottom - top) //2
+
+poin_i_0_of_cir = center_i # im.shape[0] // 2
+ 
+poin_j_0_of_cir = center_j #im.shape[1] // 2
+
+
+for i in range(pix_r_MAX ,im.shape[0]-pix_r_MAX ):
+    
+    print(i, ' ---from--- ',im.shape[0]-pix_r_MAX)
+    
+    for j in range(pix_r_MAX,im.shape[1]-pix_r_MAX ):
+        
+        temp_dec = dec(poin_i_0_of_cir,poin_j_0_of_cir,i,j)
+        
+        if temp_dec > r_Circle:
+            
+            temp_r = int(abs(temp_dec  - r_Circle) / 4 ) + 2
+            
+            if temp_r > pix_r_MAX:
+                temp_r = pix_r_MAX
+            
+            #im_res[i,j,0],im_res[i,j,1],im_res[i,j,2] =GauB_1pix(im,i,j,temp_r,sigma)
+            temp_small_im = im[i - temp_r : i + 1 + temp_r, j - temp_r : j + 1 + temp_r , : ]
+            
+            Weight_Matrix = Weight_Matrix_(sigma,temp_r)
+
+            im_res[i,j,0],im_res[i,j,1],im_res[i,j,2] = int(np.sum(temp_small_im[:,:,0] * Weight_Matrix )),int(np.sum(temp_small_im[:,:,1] * Weight_Matrix )),int(np.sum(temp_small_im[:,:,2] * Weight_Matrix ))
+        else:
+            im_res[i,j] = im[i,j]
+
+cv2.imwrite('/home/nik/Documents/Gaussian_Blur/Torvalds3.jpg', im_res)  
---
+++ gb_video.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 27 14:04:09 2022
+
+@author: nik
+@author git: https://github.com/blnikan
+
+"""
+
+import cv2
+import numpy  as np
+
+
+def gauss_func(sigma_, pos): # Gaussian function (sigma_ - sigma ; pos - coordinate  in Gaussian function)
+    return (1 / ( (2 * np.pi * sigma_ ** 2) ** 0.5 ) ) * np.e ** (-((pos ** 2)/(2  * sigma_ ** 2)))
+
+def Weight_Matrix_(sigma_, rad_):
+    
+    pos_x = np.zeros([2*rad_+1,2*rad_+1])
+    pos_y = np.zeros([2*rad_+1,2*rad_+1])
+    tepm_x = -rad_
+    #temp_y = 0
+    for i in range(pos_x.shape[0]):
+        
+        for j in range(pos_x.shape[1]):
+            pos_x[j,i] = tepm_x
+            pos_y[i,j] = -1*tepm_x
+                        
+        tepm_x += 1
+    #print("pos_x\n",pos_x)
+    #print('pos_y\n',pos_y)
+    res = gauss_func(sigma_,pos_x) * gauss_func(sigma_,pos_y) 
+    
+    return res / np.sum(res)
+def padding(img,rad_):
+    H = img.shape[0]
+    W = img.shape[1]
+    C = 3
+    
+    res = np.zeros((H+rad_*2,W+rad_*2,C))
+    res[rad_:H+rad_, rad_:W+rad_] = img
+    
+    #Pad the first/last two col and row
+    res[rad_:H+rad_,0:rad_,:C]=img[:,0:1,:C]
+    res[H+rad_:H+rad_*2,rad_:W+rad_,:]=img[H-1:H,:,:]
+    res[rad_:H+rad_,W+rad_:W+rad_*2,:]=img[:,W-1:W,:]
+    res[0:rad_,rad_:W+rad_,:C]=img[0:1,:,:C]
+    
+    #Pad the missing eight points
+    res[0:rad_,0:rad_,:C]=img[0,0,:C]
+    res[H+rad_:H+rad_*2,0:rad_,:C]=img[H-1,0,:C]
+    res[H+rad_:H+rad_*2,W+rad_:W+rad_*2,:C]=img[H-1,W-1,:C]
+    res[0:rad_,W+rad_:W+rad_*2,:C]=img[0,W-1,:C]
+    
+    
+    return res
+
+
+vid = cv2.VideoCapture('/home/nik/Documents/Gaussian_Blur/Israel.MP4')
+
+pix_r = 7
+sigma = 3
+
+if (vid.isOpened()== False): 
+  print("Error opening video stream or file")
+
+frames = []   
+
+cou = 0
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
+
+while(vid.isOpened()):
+    ret, frame = vid.read()
+    
+    if ret:
+        cou += 1
+        print(cou)
+        H = frame.shape[0]
+        W = frame.shape[1]
+
+        im = padding(frame,pix_r)
+        
+        im_res =  np.zeros(im.shape)
+        
+        Weight_Matrix = Weight_Matrix_(sigma,pix_r)
+        
+        for i in range (pix_r,im.shape[0] - pix_r):
+            #print(f'i: {i} from im.shape[0] - pix_r')
+
+            for j in range (pix_r,im.shape[1] - pix_r):
+                #0-rojo (red)
+                #1-verde (green)
+                #2-azul (blue)
+
+                temp_small_im = im[i - pix_r : i + 1 + pix_r, j - pix_r : j + 1 + pix_r , : ]
+                im_res[i,j,0],im_res[i,j,1],im_res[i,j,2] = int(np.sum(temp_small_im[:,:,0] * Weight_Matrix )),int(np.sum(temp_small_im[:,:,1] * Weight_Matrix )),int(np.sum(temp_small_im[:,:,2] * Weight_Matrix ))
+
+        #frame = im_res
+        out.write(im_res[])
+        frames.append(frame)
+        
+        #cv2.imshow('Frame',frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+        	
+            break
+        
+        if cou == 10:
+            break
+
+
+# When everything done, release the video capture object
+vid.release()
+ 
+# Closes all the frames
+cv2.destroyAllWindows()
